ed413c606b35427cefd4a8c0529d66076b566075
Colon1al <vovann631@gmail.com>
2021-10-09 07:49:11 +0300

    added basics for db class

--- sql_lib/postgresql.py
+++ sql_lib/postgresql.py
@@ -1,10 +1,29 @@
 import psycopg2 as pgsql
- 
-def connect(dbname, user, password, address):
-    """
 
-    """
-    conn = pgsql.connect(dbname='database', user='db_user', 
-                            password='mypassword', host='localhost')
-    cursor = conn.cursor()
-    return (conn, cursor)
+
+
+class database:
+
+	def __init__(self, db_name, user, password, host):
+		self.db_name = db_name
+		self.user = user
+		self.passwd = password
+		self.host = host
+		self.conn = None
+		self.curr = None
+	def connect(self):
+		"""
+
+		"""
+		conn = pgsql.connect(dbname=self.db_name, user=self.user, 
+								password=self.passwd, host=self.host)
+		cursor = conn.cursor()
+		self.conn = conn
+		self.curr = cursor
+		return (conn, cursor)
+
+	def getUserData(self, user_id):
+		pass
+
+	def writeUserData(self, user_id, service, password):
+		pass
